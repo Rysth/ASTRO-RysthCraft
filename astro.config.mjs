c9578287887bd9e35d31090f2a79166959f46950
Rysth <johnpalacios.t@gmail.com>
2024-02-09 20:20:40 -0500

    Installed astro sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://rysthdesign.com",
+  integrations: [tailwind(), react(), sitemap()],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
